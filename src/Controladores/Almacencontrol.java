bcbf165e457eb1351617bd23c434bcae3dba1179
CMArantxa <aranzazucano@hotmail.com>
2024-01-16 20:08:08 +0100

    creo modificar proveedor

--- src/Controladores/Almacencontrol.java
+++ src/Controladores/Almacencontrol.java
@@ -9,18 +9,19 @@ import java.util.List;
 
 public class Almacencontrol {
 
-private List<Proveedor>proveedorList;
-private List<Piezas>piezasList;
-private List<Pedido>pedidoList;
+    private List<Proveedor> proveedorList;
+    private List<Piezas> piezasList;
+    private List<Pedido> pedidoList;
 
     public Almacencontrol() {
-        proveedorList=new ArrayList<>();
-        piezasList=new ArrayList<>();
-        pedidoList=new ArrayList<>();
+        proveedorList = new ArrayList<>();
+        piezasList = new ArrayList<>();
+        pedidoList = new ArrayList<>();
 
     }
-    public boolean nuevoProveedor(String cif,String nombre,String direccion,String localidad,String provincia){
-        Proveedor proveedor=new Proveedor(cif,nombre);
+
+    public boolean nuevoProveedor(String cif, String nombre, String direccion, String localidad, String provincia) {
+        Proveedor proveedor = new Proveedor(cif, nombre);
         proveedor.setDireccion(direccion);
         proveedor.setLocalidad(localidad);
         proveedor.setProvincia(provincia);
@@ -29,22 +30,57 @@ private List<Pedido>pedidoList;
 
     @Override
     public String toString() {
-        return "Almacencontrol{" +"\n"+
-                "proveedorList=" + proveedorList + "\n"+
-                ", piezasList=" + piezasList +"\n"+
-                ", pedidoList=" + pedidoList +"\n"+
-                '}'+"\n";
+        return "Almacencontrol{" + "\n" +
+                "proveedorList=" + proveedorList + "\n" +
+                ", piezasList=" + piezasList + "\n" +
+                ", pedidoList=" + pedidoList + "\n" +
+                '}' + "\n";
     }
 
-    public void borrarProveedor(String cif) {
-        int ProvedorList = 0;
-        for (int i = 0; i < proveedorList.size(); i++) {
-         if (cif.equals(proveedorList.get(i).getCif())){
-             proveedorList.remove(i);
-             System.out.println("Proveedor eliminado.");
-         }
+    /*
+        public void borrarProveedor(String cif) {
+            int ProvedorList = 0;
+            for (int i = 0; i < proveedorList.size(); i++) {
+             if (cif.equals(proveedorList.get(i).getCif())){
+                 proveedorList.remove(i);
+                 System.out.println("Proveedor eliminado.");
+             }
+            }
         }
+
+     */
+    public boolean borrarProveedor(String cif) {
+        return proveedorList.removeIf(proveedor -> cif.equals(proveedor.getCif()));
+    }
+/*
+    public boolean modificarProveedor(String cif, String nombre, String direccion, String localidad, String provincia) {
+        for (int i = 0; i < proveedorList.size(); i++) {
+            if (cif.equals(proveedorList.get(i).getCif())) {
+                proveedorList.get(i).setCif(cif);
+                proveedorList.get(i).setNombre(nombre);
+                proveedorList.get(i).setDireccion(direccion);
+                proveedorList.get(i).setLocalidad(localidad);
+                proveedorList.get(i).setProvincia(provincia);
+            }
+            return true;
+        }return false
     }
 
+ */
+    public boolean modificarProveedor(String cif, String nombre, String direccion, String localidad, String provincia) {
+        for (Proveedor p : proveedorList) {
+            if (cif.equals(p.getCif())) {
+                p.setCif(cif);
+                p.setNombre(nombre);
+                p.setDireccion(direccion);
+                p.setLocalidad(localidad);
+                p.setProvincia(provincia);
+                return true;
+            }
+
+        }
+        return false;
+    }
 }
+
 
--- src/com/ceica/Main.java
+++ src/com/ceica/Main.java
@@ -8,12 +8,17 @@ public class Main {
         almacen.nuevoProveedor("a","nombre","dir","loc","pro");
         almacen.nuevoProveedor("b","bnombre","bdir","bloc","bpro");
         System.out.println(almacen.toString());
+        almacen.modificarProveedor("a","nombre","direccion","localidad","provincia");
+        /*
+        System.out.println(almacen.toString());
         almacen.borrarProveedor("a");
         System.out.println(almacen.toString());
         almacen.borrarProveedor("b");
         System.out.println(almacen.toString());
         almacen.borrarProveedor("c");
         System.out.println(almacen.toString());
+
+         */
     }
 
 }
