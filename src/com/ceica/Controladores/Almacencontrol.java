99b92b20418d31f10eec2cdb259310d7b9ba1d66
CMArantxa <aranzazucano@hotmail.com>
2024-01-19 20:19:28 +0100

    interfaz de usuario

--- src/com/ceica/Controladores/Almacencontrol.java
+++ src/com/ceica/Controladores/Almacencontrol.java
@@ -6,6 +6,10 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Es el cuerpo de métodos a utilizar en main.
+ * Accede a todos los listados.
+ */
 public class Almacencontrol {
 
     private List<Proveedor> proveedorList;
@@ -21,6 +25,16 @@ public class Almacencontrol {
         categorias.add(new Categoria(2,"mediano"));
         categorias.add(new Categoria(3,"grande"));
     }
+
+    /**
+     * @param cif
+     * @param nombre
+     * @param direccion
+     * @param localidad
+     * @param provincia
+     * @return
+     * Metodo para dar de alta nuevos proveedores.
+     */
     public boolean nuevoProveedor(String cif, String nombre, String direccion, String localidad, String provincia) {
         Proveedor proveedor = new Proveedor(cif, nombre);
         proveedor.setDireccion(direccion);
@@ -38,6 +52,11 @@ public class Almacencontrol {
                 '}' + "\n";
     }
 
+    /**
+     * @param cif
+     * @return
+     * Metodo que busca el proveedor por cif y permite borrarlo.
+     */
     /*
         public void borrarProveedor(String cif) {
             int ProvedorList = 0;
@@ -69,7 +88,16 @@ public class Almacencontrol {
 
  */
 
-    public boolean modificarProveedor(String cif, String nombre, String direccion, String localidad, String provincia) {
+    /**
+     * @param cif
+     * @param nombre
+     * @param direccion
+     * @param localidad
+     * @param provincia
+     * @return
+     * Metodo que busca un proveedor por cif y permite modificar los datos guardados en los parametros.
+     */
+    public boolean modificarNombreProveedorByCIF(String cif, String nombre) {
      /*
         for (Proveedor p : proveedorList) {
             if (cif.equals(p.getCif())) {
@@ -96,18 +124,72 @@ public class Almacencontrol {
                 .orElse(false);
 
     }
+    public boolean modificarDireccionProveedorByCIF(String cif, String direccion) {
+        return proveedorList.stream()
+                .filter(p -> cif.equals(p.getCif()))
+                .findFirst()
+                .map(p ->
+                {
+                    p.setDireccion(direccion);
+                    return true;
+                })
+                .orElse(false);
+    }
+    public boolean modificarlocalidadProveedorByCIF(String cif, String localidad) {
+        return proveedorList.stream()
+                .filter(p -> cif.equals(p.getCif()))
+                .findFirst()
+                .map(p ->
+                {
+                    p.setLocalidad(localidad);
+                    return true;
+                })
+                .orElse(false);
+    }
+    public boolean modificarProvinciaProveedorByCIF(String cif, String provincia) {
+        return proveedorList.stream()
+                .filter(p -> cif.equals(p.getCif()))
+                .findFirst()
+                .map(p ->
+                {
+                    p.setProvincia(provincia);
+                    return true;
+                })
+                .orElse(false);
+    }
+    /**
+     * @param nombre
+     * @param color
+     * @param precio
+     * @param idcategoria
+     * @return
+     * Metodo que da de alta una pieza.
+     */
 public boolean nuevaPieza (String nombre,Color color,Double precio,int idcategoria){
         Piezas pieza=new Piezas(nombre,color.toString() ,precio);
         pieza.setCategoria(getCategoriaById(idcategoria));
         piezasList.add(pieza);
         return true;
 }
+
+    /**
+     * @param id
+     * @return
+     * Metodo para buscar la categoria de una pieza a traves del id de la pieza.
+     */
     private Categoria getCategoriaById(int id){
         return categorias.stream().filter(c->c.getId()==id).findFirst().get();
     }
-    public boolean cambiarPrecio(int idcategoria,Double precio){
+
+    /**
+     * @param idpieza
+     * @param precio
+     * @return
+     * Metodo para modificar el parametro precio buscando por id de pieza.
+     */
+    public boolean cambiarPrecio(int idpieza,Double precio){
         return piezasList.stream()
-                .filter(p -> idcategoria==(p.getId()))
+                .filter(p -> idpieza==(p.getId()))
                 .findFirst()
                 .map(p ->
                 {
@@ -116,6 +198,15 @@ public boolean nuevaPieza (String nombre,Color color,Double precio,int idcategor
                 })
                 .orElse(false);
     }
+
+    /**
+     * @param cif
+     * @param idpieza
+     * @param cantidad
+     * @return
+     * Metodo para grabar nuevos pedidos en una lista.
+     * Accede buscando el cif del proveedor.
+     */
     public String nuevoPedido(String cif,int idpieza,int cantidad){
 
         Proveedor proveedor=getProveedorByCIF(cif);
@@ -136,6 +227,12 @@ public boolean nuevaPieza (String nombre,Color color,Double precio,int idcategor
         }
         return "Error,proveedor no existe";
     }
+
+    /**
+     * @param cif
+     * @return
+     * Metodo de busqueda de proveedor por cif.
+     */
     private Proveedor getProveedorByCIF(String cif){
         for (Proveedor p:proveedorList) {
             if (cif.equals(p.getCif())){
@@ -144,7 +241,13 @@ public boolean nuevaPieza (String nombre,Color color,Double precio,int idcategor
         }
         return null;
     }
-    private Piezas getPiezasByID(int id) {
+
+    /**
+     * @param id
+     * @return
+     * Metodo de busqueda de pieza por id.
+     */
+    public Piezas getPiezasByID(int id) {
         for (int i = 0; i < piezasList.size(); i++) {
             if (piezasList.get(i).getId() == id) {
                 return piezasList.get(i);
@@ -152,6 +255,12 @@ public boolean nuevaPieza (String nombre,Color color,Double precio,int idcategor
         }
         return null;
     }
+
+    /**
+     * @param idpieza
+     * @return
+     * Metodo que muestra el listado de pedidos de una pieza concreta.
+     */
     public String getPedidosByPieza(int idpieza) {
         List<Pedido> pedidosByPieza = new ArrayList<>();
         for (Pedido pedido : pedidoList) {
@@ -165,6 +274,12 @@ public boolean nuevaPieza (String nombre,Color color,Double precio,int idcategor
             return "No hay pedidos de esa pieza";
         }
     }
+
+    /**
+     * @param cif
+     * @return
+     * Metodo que muestra el listado de pedidos de un proveedor.
+     */
     public String getPedidosByProveedor(String cif) {
         List<Pedido> pedidosByCIF = new ArrayList<>();
         for (Pedido pedido : pedidoList) {
@@ -179,6 +294,17 @@ public boolean nuevaPieza (String nombre,Color color,Double precio,int idcategor
         }
     }
 
+    public String verProveedores() {
+        return proveedorList.toString();
+    }
+    public boolean buscarProveedorCIF(String cif){
+        for (Proveedor p:proveedorList) {
+            if (cif.equals(p.getCif())){
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 
--- src/com/ceica/Main.java
+++ src/com/ceica/Main.java
@@ -1,22 +1,201 @@
 package com.ceica;
 
 import com.ceica.Controladores.Almacencontrol;
+import com.ceica.Controladores.Logincontroller;
 import com.ceica.Modelos.Color;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Almacencontrol almacen=new Almacencontrol();
-        almacen.nuevoProveedor("a","nombre","dir","loc","pro");
-        almacen.nuevaPieza("piezaA", Color.AZUL,45.5,1);
-        almacen.nuevaPieza("piezaB",Color.AMARILLO,30.00,2);
-        System.out.println(almacen.toString());
-        almacen.cambiarPrecio(1,29.99);
-        System.out.println(almacen.nuevoPedido("a",1,50));
-        System.out.println(almacen.getPedidosByPieza(1));
-        System.out.println(almacen.getPedidosByProveedor("a"));
-        System.out.println(almacen.toString());
-
-
+        String usr,pass;
+        Almacencontrol almacen = new Almacencontrol();
+        Scanner leer=new Scanner(System.in);
+        System.out.println("Bienvenido a AppAlmacen");
+        System.out.println("Pulse enter para empezar");
+        leer.nextLine();
+        do {
+            System.out.println("Login");
+            System.out.println("introduce usuario: ");
+            usr= leer.nextLine();
+            System.out.println("introduce la password");
+            pass= leer.nextLine();
+            if(Logincontroller.login(usr,pass)){
+                System.out.println("estoy en AppAlmacen");
+                menuPrincipalAlmacen(leer,almacen);
+            }else{
+                System.out.println("usuario o contraseña incorrecta");
+            }
+        }while (true);
     }
 
+    private static void menuPrincipalAlmacen(Scanner leer, Almacencontrol almacen) {
+       String op = "";
+        String menuPrincipal= """
+                1.Proveedores.
+                2.Piezas.
+                3.Pedidos.
+                4.Salir.
+                """;
+        do {
+            System.out.println(menuPrincipal);
+            op= leer.nextLine();
+            switch (op){
+                case "1":
+                    subMenuProveedores(leer,almacen);
+                    break;
+                case "2":
+                    submenuPiezas(leer,almacen);
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    System.out.println("saliendo...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }while(!"4".equals(op));
+    }
+
+    private static void submenuPiezas(Scanner leer, Almacencontrol almacen) {
+        String op,nombre,color;
+        int id,categoria;
+        Double precio;
+        String menuPiezas= """
+                1.Dar de alta pieza.
+                2.Cambiar precio
+                3.Eliminar pieza.
+                4.Salir
+                """;
+        do {
+            System.out.println(menuPiezas);
+            op=leer.nextLine();
+            switch (op) {
+                case "1":
+                    almacen.nuevaPieza();
+
+                    System.out.println("Nombre");
+                    nombre = leer.nextLine();
+                    System.out.println("color");
+                    color = leer.nextLine();
+                    System.out.println("precio");
+                    precio = Double.valueOf(leer.nextLine());
+                    break;
+                case "2":
+                    System.out.println("ID");
+                    id = leer.nextInt();
+                    System.out.println("introduce el nuevo precio");
+                    precio = leer.nextDouble();
+                    almacen.cambiarPrecio(id, precio);
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+                    System.out.println("saliendo");
+                default:
+                    System.out.println("Opcion no valida");
+            }
+
+        } while (!"4".equals(op));
+    }
+
+    private static void subMenuProveedores(Scanner leer, Almacencontrol almacen) {
+        String op, cif, nombre, direccion, localidad, provincia;
+        String menuProveedores = """
+                                
+                1.Nuevo proveedor
+                2.Editar proveedor
+                3.Ver proveedores
+                4.Eliminar proveedor
+                5.Volve a menú principal""";
+        do {
+            System.out.println(menuProveedores);
+            op = leer.nextLine();
+            switch (op) {
+                case "1":
+                    System.out.println("CIF");
+                    cif=leer.nextLine();
+                    System.out.println("Nombre");
+                    nombre=leer.nextLine();
+                    System.out.println("Direccion");
+                    direccion= leer.nextLine();
+                    System.out.println("Localidad");
+                    localidad= leer.nextLine();
+                    System.out.println("Provincia");
+                    provincia= leer.nextLine();
+                    almacen.nuevoProveedor(cif,nombre,direccion,localidad,provincia);
+                    break;
+                case "2":
+                  System.out.println("Introduce el cif");
+                    cif= leer.nextLine();
+                    if (almacen.buscarProveedorCIF(cif)){
+                        editarProveedor(leer,almacen,cif);
+                    }else{
+                        System.out.println("Proveedor no existe");
+                    }
+                    break;
+                case "3":
+                    System.out.println(almacen.verProveedores());
+                    break;
+                case "4":
+                    System.out.println("introduce el CIF");
+                    cif= leer.nextLine();
+                    if (almacen.buscarProveedorCIF(cif)){
+                      almacen.borrarProveedor(cif);
+                    }else {
+                        System.out.println("No existe proveedor con ese cif");
+                    }
+                    break;
+                case "5":
+                    System.out.println("volver a menu principal");
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }while(!"5".equals(op));
+            }
+
+    private static void editarProveedor(Scanner leer, Almacencontrol almacen, String cif) {
+        String op,dato;
+        String editarProveedor= """
+                1.Editar nombre
+                2.Editar direccion
+                3.Editar localidad
+                4.Editar provincia
+                5.Salir
+                """;
+        do {
+            System.out.println(editarProveedor);
+            op=leer.nextLine();
+            switch (op) {
+                case "1":
+                    System.out.println("dime el nuevo nombre");
+                    dato = leer.nextLine();
+                    almacen.modificarNombreProveedorByCIF(cif, dato);
+                    break;
+
+                case "2":
+                    System.out.println("dime la nueva direccion");
+                    dato = leer.nextLine();
+                    almacen.modificarDireccionProveedorByCIF(cif, dato);
+                    break;
+                case "3":
+                    System.out.println("dime la nueva localidad");
+                    dato = leer.nextLine();
+                    almacen.modificarlocalidadProveedorByCIF(cif, dato);
+
+                    break;
+                case "4":
+                    System.out.println("dime la nueva provincia");
+                    dato = leer.nextLine();
+                    almacen.modificarProvinciaProveedorByCIF(cif, dato);
+                    break;
+                case "5":
+                    System.out.println("salir");
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }while (! "5".equals(op));
+    }
 }
